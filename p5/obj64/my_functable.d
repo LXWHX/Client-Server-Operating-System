fb8ab64023ccb67fb415aaf73366c1e104abb7ab
Shiqian Li <70154023+LXWHX@users.noreply.github.com>
2025-01-18 15:33:03 -0500

    Add files via upload

---
+++ p5/obj64/my_functable.d
@@ -0,0 +1,3 @@
+obj64/my_functable.o: server/my_functable.cc server/../common/constants.h \
+ server/../common/contextmanager.h server/../common/err.h \
+ server/../common/file.h server/functable.h server/functypes.h
---
+++ p5/obj64/my_mapreduce.d
@@ -0,0 +1,3 @@
+obj64/my_mapreduce.o: server/my_mapreduce.cc server/../common/constants.h \
+ server/../common/contextmanager.h server/functable.h server/functypes.h \
+ server/map.h server/storage.h
